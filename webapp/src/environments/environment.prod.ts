dccf49272cba66ee93c68964bcc90ce3b5dc8870
Dancs Kornél <113311203+dancs-kornel@users.noreply.github.com>
2025-12-27 21:19:36 +0100

    Set production API URL

--- webapp/src/environments/environment.prod.ts
+++ webapp/src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:8080/api'  // TODO: Update this to production API URL
+  apiUrl: 'https://regexpath-api-hzebawcrc5arhkbj.francecentral-01.azurewebsites.net/api'
 };
+
